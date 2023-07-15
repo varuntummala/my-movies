8a8a1a1263e1638d710d7bea05f6508b708e6d8f
Varun Tummala <varunt@gmail.com>
2023-07-15 19:36:09 -0400

    Movies App2

--- my-movies/src/App.jsx
+++ my-movies/src/App.jsx
@@ -1,22 +1,20 @@
-import Movie from "./Movie.jsx";
+import Collection from "./Collection.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./Collection.css";
-import PropTypes from "prop-types";
 
-function Collection(props) {
+function App() {
     return (
         <>
-            <div className="collection container board  mt-3 justify-center">
+            <div className="container board mt-3 justify-center">
                 <div className="row text-center">
-                    <h1>{props.text} Movie List:</h1>
+                    <h1>My Favourite Movie Collections</h1>
                     <p>
-                        Click Add to add a new Movie and click a Movie to cross it off!
+                        Click Add to add a new Movie Collection and click a Collection to cross it off!
                     </p>
                 </div>
                 <div className="row justify-center text-center">
-                    <Movie text={props.movies[0]}/>
-                    <Movie text={props.movies[1]}/>
-                    <Movie text={props.movies[2]}/>
+                    <Collection text={"Action Collection"} movies={["Maverick","Presidioo","Package"]}/>
+                    <Collection text={"Romance Collection"} movies={["Casablanca","Romeo and Juliet","Life is Beautiful"]}/>
+                    <Collection text={"War Collection"} movies={["Dunkirk","1917","Imitation Game"]}/>
                 </div>
                 <div className="row mt-3 d-flex justify-content-center">
                     <div className="col-md-6 ">
@@ -24,12 +22,12 @@ function Collection(props) {
                             <input
                                 type="text"
                                 className="form-control"
-                                placeholder="Write Movie here..."
-                                aria-label="Movie"
+                                placeholder="Write Collection Name here..."
+                                aria-label="CollectionInput"
                             />
                             <div className="input-group-append">
                                 <button className="btn btn-primary h-100 m-0" type="button">
-                                    Add
+                                    Add a Movie Collection
                                 </button>
                             </div>
                         </div>
@@ -40,20 +38,4 @@ function Collection(props) {
     );
 }
 
-Collection.defaultProps = {
-    movies: ["Untitled Movie","Untitled Movie","Untitled Movie"]
-}
-
-Collection.propTypes = {
-    movies: PropTypes.isRequired,
-};
-
-Collection.defaultProps = {
-    text: "Untitled To Do Item"
-}
-
-Collection.propTypes = {
-    text: PropTypes.string.isRequired,
-};
-
-export default Collection;
+export default App;
